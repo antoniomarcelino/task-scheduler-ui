5524b1d5faa62b878ea2bf8ca80e407df7074298
antoniomarcelino <antoniomarcelino@live.com>
2020-12-07 19:04:03 -0300

    #1 MSG: Flex Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgxMaskModule } from 'ngx-mask'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgxMaskModule.forRoot(),
+    FlexLayoutModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
